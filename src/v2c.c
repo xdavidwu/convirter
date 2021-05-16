4bccd8cb4ebda5c5434fe9e6c494009a8afe395d
xdavidwu <xdavidwuph@gmail.com>
2021-05-16 21:07:30 +0800

    v2c: refactor with state struct
    say goodbye to globals

--- src/v2c.c
+++ src/v2c.c
@@ -31,15 +31,16 @@ static const char *temporary_paths[] = {
 
 static const int bufsz = 3 * 1024 * 1024;
 
-static struct archive_entry *entry = NULL;
+struct v2c_state {
+	guestfs_h *guestfs;
+	struct archive *layer_archive;
+	struct archive_entry *layer_entry;
+	struct archive_entry_linkresolver *layer_link_resolver;
+	time_t modification_start, modification_end, source_date_epoch;
+	bool set_modification_epoch;
+};
 
-static struct archive_entry_linkresolver *resolver = NULL;
-
-static bool set_modification_epoch = false;
-
-static time_t modification_start, modification_end, source_date_epoch;
-
-static int dump_file_content(guestfs_h *g, const char *path, struct archive *archive, int64_t size) {
+static int dump_file_content(struct v2c_state *state, const char *path, int64_t size) {
 	// guestfs_read_file does guestfs_download and reads the whole file into memory
 	// read by our own to control buffer size
 	// guestfs_download have it's own (allocated and freed at each batch) buffer and extra IO
@@ -48,13 +49,14 @@ static int dump_file_content(guestfs_h *g, const char *path, struct archive *arc
 	size_t read = 0;
 	int res = 0;
 	while (size) {
-		char *buf = guestfs_pread(g, path, size > bufsz ? bufsz : size, offset, &read);
+		char *buf = guestfs_pread(state->guestfs, path,
+			size > bufsz ? bufsz : size, offset, &read);
 		if (!buf) {
 			return -errno;
 		}
 		size -= read;
 		offset += read;
-		res = archive_write_data(archive, buf, read);
+		res = archive_write_data(state->layer_archive, buf, read);
 		free(buf);
 		if (res < 0) {
 			return -errno;
@@ -63,13 +65,13 @@ static int dump_file_content(guestfs_h *g, const char *path, struct archive *arc
 	return 0;
 }
 
-static int dump_to_archive(guestfs_h *g, const char *path,
-		struct guestfs_statns *stat, struct archive *archive) {
+static int dump_to_archive(struct v2c_state *state, const char *path,
+		struct guestfs_statns *stat) {
 	if (!stat) {
 		return -errno;
 	}
-	archive_entry_clear(entry);
-	archive_entry_set_pathname(entry, &path[1]);
+	archive_entry_clear(state->layer_entry);
+	archive_entry_set_pathname(state->layer_entry, &path[1]);
 	struct stat tmpstat = {
 		.st_dev = stat->st_dev,
 		.st_ino = stat->st_ino,
@@ -92,69 +94,70 @@ static int dump_to_archive(guestfs_h *g, const char *path,
 		},
 	};
 
-	if (set_modification_epoch) {
-		if (tmpstat.st_atim.tv_sec >= modification_start &&
-				tmpstat.st_atim.tv_sec <= modification_end) {
-			tmpstat.st_atim.tv_sec = source_date_epoch;
+	if (state->set_modification_epoch) {
+		if (tmpstat.st_atim.tv_sec >= state->modification_start &&
+				tmpstat.st_atim.tv_sec <= state->modification_end) {
+			tmpstat.st_atim.tv_sec = state->source_date_epoch;
 			tmpstat.st_atim.tv_nsec = 0;
 		}
-		if (tmpstat.st_mtim.tv_sec >= modification_start &&
-				tmpstat.st_mtim.tv_sec <= modification_end) {
-			tmpstat.st_mtim.tv_sec = source_date_epoch;
+		if (tmpstat.st_mtim.tv_sec >= state->modification_start &&
+				tmpstat.st_mtim.tv_sec <= state->modification_end) {
+			tmpstat.st_mtim.tv_sec = state->source_date_epoch;
 			tmpstat.st_mtim.tv_nsec = 0;
 		}
-		if (tmpstat.st_ctim.tv_sec >= modification_start &&
-				tmpstat.st_ctim.tv_sec <= modification_end) {
-			tmpstat.st_ctim.tv_sec = source_date_epoch;
+		if (tmpstat.st_ctim.tv_sec >= state->modification_start &&
+				tmpstat.st_ctim.tv_sec <= state->modification_end) {
+			tmpstat.st_ctim.tv_sec = state->source_date_epoch;
 			tmpstat.st_ctim.tv_nsec = 0;
 		}
 	}
 
-	archive_entry_copy_stat(entry, &tmpstat);
+	archive_entry_copy_stat(state->layer_entry, &tmpstat);
 	if ((stat->st_mode & S_IFMT) == S_IFLNK) {
-		char *link = guestfs_readlink(g, path);
+		char *link = guestfs_readlink(state->guestfs, path);
 		if (!link) {
 			free(link);
 			return -errno;
 		}
-		archive_entry_set_symlink(entry, link);
+		archive_entry_set_symlink(state->layer_entry, link);
 		free(link);
 	}
 
-	struct guestfs_xattr_list *xattrs = guestfs_lgetxattrs(g, path);
+	struct guestfs_xattr_list *xattrs = guestfs_lgetxattrs(state->guestfs, path);
 	for (int i = 0; i < xattrs->len; i++) {
-		archive_entry_xattr_add_entry(entry, xattrs->val[i].attrname,
-			xattrs->val[i].attrval, xattrs->val[i].attrval_len);
+		archive_entry_xattr_add_entry(state->layer_entry,
+			xattrs->val[i].attrname, xattrs->val[i].attrval,
+			xattrs->val[i].attrval_len);
 	}
 	guestfs_free_xattr_list(xattrs);
 
 	// in pax, data is stored on first seen, and archive_entry_linkify
 	// *sparse is for format where data is stored at last seen.
 	struct archive_entry *dummy;
-	archive_entry_linkify(resolver, &entry, &dummy);
-	archive_write_header(archive, entry);
+	archive_entry_linkify(state->layer_link_resolver, &state->layer_entry, &dummy);
+	archive_write_header(state->layer_archive, state->layer_entry);
 
 	// size is set to zero if linkify found hardlink to previous entry
-	if ((stat->st_mode & S_IFMT) == S_IFREG && archive_entry_size(entry)) {
-		dump_file_content(g, path, archive, stat->st_size);
+	if ((stat->st_mode & S_IFMT) == S_IFREG && archive_entry_size(state->layer_entry)) {
+		dump_file_content(state, path, stat->st_size);
 	}
 	return 0;
 }
 
-static int dump_guestfs(guestfs_h *g, const char *dir, struct archive *archive) {
+static int dump_guestfs(struct v2c_state *state, const char *dir) {
 	for (int i = 0; temporary_paths[i]; i++) {
 		if (!strcmp(dir, temporary_paths[i])) {
 			return 0;
 		}
 	}
 
-	char **ls = guestfs_ls(g, dir);
+	char **ls = guestfs_ls(state->guestfs, dir);
 	if (!ls) {
 		return -errno;
 	} else if (!ls[0]) {
 		return 0;
 	}
-	struct guestfs_statns_list *stats = guestfs_lstatnslist(g, dir, ls);
+	struct guestfs_statns_list *stats = guestfs_lstatnslist(state->guestfs, dir, ls);
 	if (!stats) {
 		return -errno;
 	}
@@ -187,14 +190,14 @@ static int dump_guestfs(guestfs_h *g, const char *dir, struct archive *archive) 
 		}
 		free(ls[i]);
 
-		res = dump_to_archive(g, full, &stats->val[i], archive);
+		res = dump_to_archive(state, full, &stats->val[i]);
 		if (res < 0) {
 			free(full);
 			goto cleanup;
 		}
 
 		if ((stats->val[i].st_mode & S_IFMT) == S_IFDIR) {
-			res = dump_guestfs(g, full, archive);
+			res = dump_guestfs(state, full);
 			if (res < 0) {
 				free(full);
 				goto cleanup;
@@ -212,32 +215,34 @@ static int mounts_cmp(const void *a, const void *b) {
 	return strcmp(*(const char **)a, *(const char **)b);
 }
 
-static int cleanup_fstab(guestfs_h *g, char **mounts) {
+static int cleanup_fstab(struct v2c_state *state, char **mounts) {
 	/* TODO: make it optional */
-	guestfs_aug_init(g, "/", 0);
+	guestfs_aug_init(state->guestfs, "/", 0);
 	int index = 0, res = 0;
 	while (mounts[index]) {
 		char exp[27 + strlen(mounts[index]) + 1];
 		// TODO: also match devices?
 		// needs to consider: UUID, PARTUUID, LABEL, PARTLABEL
 		sprintf(exp, "/files/etc/fstab/*[file='%s']", mounts[index]);
-		res = guestfs_aug_rm(g, exp);
+		res = guestfs_aug_rm(state->guestfs, exp);
 		if (res < 0) {
 			fprintf(stderr, "Error: fstab cleanup mount %s at %s failed.\n",
 				mounts[index + 1], mounts[index]);
-			return res;
+			goto cleanup;
 		}
 		index += 2;
 	}
-	res = guestfs_aug_save(g);
+	res = guestfs_aug_save(state->guestfs);
 	if (res < 0) {
 		fprintf(stderr, "Error: saving edited fstab failed.\n");
-		return res;
+		goto cleanup;
 	}
-	return guestfs_aug_close(g);
+cleanup:
+	guestfs_aug_close(state->guestfs);
+	return res;
 }
 
-static int cleanup_systemd(guestfs_h *g) {
+static int cleanup_systemd(struct v2c_state *state) {
 	/* TODO: organize by category (e.g. network) and make them optional */
 	// Debian ifup
 	{
@@ -247,7 +252,7 @@ static int cleanup_systemd(guestfs_h *g) {
 			"networking.service",
 			NULL,
 		};
-		free(guestfs_command(g, cmd));
+		free(guestfs_command(state->guestfs, cmd));
 	}
 	// Ubuntu multipathd
 	{
@@ -257,7 +262,7 @@ static int cleanup_systemd(guestfs_h *g) {
 			"multipathd.service",
 			NULL,
 		};
-		free(guestfs_command(g, cmd));
+		free(guestfs_command(state->guestfs, cmd));
 	}
 	// Ubuntu systemd-rfkill (socket)
 	{
@@ -267,7 +272,7 @@ static int cleanup_systemd(guestfs_h *g) {
 			"systemd-rfkill.socket",
 			NULL,
 		};
-		free(guestfs_command(g, cmd));
+		free(guestfs_command(state->guestfs, cmd));
 	}
 	// CentOS auditd
 	// Only one instance should run, leave it to host
@@ -279,7 +284,7 @@ static int cleanup_systemd(guestfs_h *g) {
 			"auditd.service",
 			NULL,
 		};
-		free(guestfs_command(g, cmd));
+		free(guestfs_command(state->guestfs, cmd));
 	}
 	return 0;
 }
@@ -301,6 +306,7 @@ static int setup_systemd_config(struct cvirt_oci_config *config) {
 }
 
 int main(int argc, char *argv[]) {
+	struct v2c_state state = {0};
 	int res = 0;
 	if (argc != 3) {
 		puts(usage);
@@ -309,24 +315,25 @@ int main(int argc, char *argv[]) {
 
 	char *source_date_epoch_env = getenv("SOURCE_DATE_EPOCH");
 	if (source_date_epoch_env) {
-		set_modification_epoch = true;
-		source_date_epoch = atoll(source_date_epoch_env);
+		state.set_modification_epoch = true;
+		state.source_date_epoch = atoll(source_date_epoch_env);
 	}
 
-	guestfs_h *g = guestfs_create();
-	if (!g) {
+	state.guestfs = guestfs_create();
+	if (!state.guestfs) {
 		exit(EXIT_FAILURE);
 	}
-	res = guestfs_add_drive_opts(g, argv[1], GUESTFS_ADD_DRIVE_OPTS_READONLY, 1, -1);
+	res = guestfs_add_drive_opts(state.guestfs, argv[1],
+		GUESTFS_ADD_DRIVE_OPTS_READONLY, 1, -1);
 	if (res < 0) {
 		exit(EXIT_FAILURE);
 	}
-	res = guestfs_launch(g);
+	res = guestfs_launch(state.guestfs);
 	if (res < 0) {
 		exit(EXIT_FAILURE);
 	}
 
-	char **roots = guestfs_inspect_os(g);
+	char **roots = guestfs_inspect_os(state.guestfs);
 	char *target_root = NULL;
 	if (!roots) {
 		exit(EXIT_FAILURE);
@@ -335,7 +342,7 @@ int main(int argc, char *argv[]) {
 		exit(EXIT_FAILURE);
 	}
 	for (int i = 0; roots[i]; i++) {
-		char *type = guestfs_inspect_get_type(g, roots[i]);
+		char *type = guestfs_inspect_get_type(state.guestfs, roots[i]);
 		if (!type) {
 			free(roots[i]);
 			continue;
@@ -358,23 +365,24 @@ int main(int argc, char *argv[]) {
 		exit(EXIT_FAILURE);
 	}
 
-	entry = archive_entry_new();
-	if (!entry) {
+	state.layer_entry = archive_entry_new();
+	if (!state.layer_entry) {
 		perror("archive_entry_new");
 		exit(EXIT_FAILURE);
 	}
 
-	resolver = archive_entry_linkresolver_new();
-	if (!resolver) {
+	state.layer_link_resolver = archive_entry_linkresolver_new();
+	if (!state.layer_link_resolver) {
 		perror("archive_entry_linkresolver_new");
 		exit(EXIT_FAILURE);
 	}
-	struct archive *ar = cvirt_oci_layer_get_libarchive(layer);
-	archive_entry_linkresolver_set_strategy(resolver, archive_format(ar));
+	state.layer_archive = cvirt_oci_layer_get_libarchive(layer);
+	archive_entry_linkresolver_set_strategy(state.layer_link_resolver,
+		archive_format(state.layer_archive));
 
-	modification_start = time(NULL);
+	state.modification_start = time(NULL);
 
-	char **mounts = guestfs_inspect_get_mountpoints(g, target_root);
+	char **mounts = guestfs_inspect_get_mountpoints(state.guestfs, target_root);
 	free(target_root);
 	if (!mounts || !mounts[0]) {
 		exit(EXIT_FAILURE);
@@ -384,36 +392,37 @@ int main(int argc, char *argv[]) {
 	sz /= 2;
 	qsort(mounts, sz, sizeof(char *) * 2, mounts_cmp);
 	for (int index = 0; mounts[index]; index += 2) {
-		if (mounts[index][1] != '\0' && !guestfs_is_dir(g, mounts[index])) {
-			res = guestfs_mkdir_p(g, mounts[index]);
+		if (mounts[index][1] != '\0' &&
+				!guestfs_is_dir(state.guestfs, mounts[index])) {
+			res = guestfs_mkdir_p(state.guestfs, mounts[index]);
 			if (res < 0) {
 				fprintf(stderr, "Warning: mountpoint %s setup failed.\n",
 					mounts[index]);
 			}
 		}
-		res = guestfs_mount(g, mounts[index + 1], mounts[index]);
+		res = guestfs_mount(state.guestfs, mounts[index + 1], mounts[index]);
 		if (res < 0) {
 			fprintf(stderr, "Warning: mount %s at %s failed.\n",
 				mounts[index + 1], mounts[index]);
 		}
 	}
 
-	cleanup_fstab(g, mounts);
+	cleanup_fstab(&state, mounts);
 
 	for (int i = 0; mounts[i]; i++) {
 		free(mounts[i]);
 	}
 	free(mounts);
 
-	cleanup_systemd(g);
+	cleanup_systemd(&state);
 
-	modification_end = time(NULL);
+	state.modification_end = time(NULL);
 
 	// guestfs_tar_out would let whiteouts fall in
-	dump_guestfs(g, "/", ar);
+	dump_guestfs(&state, "/");
 
-	archive_entry_free(entry);
-	archive_entry_linkresolver_free(resolver);
+	archive_entry_free(state.layer_entry);
+	archive_entry_linkresolver_free(state.layer_link_resolver);
 	cvirt_oci_layer_close(layer);
 
 	struct cvirt_oci_config *config = cvirt_oci_config_new();
@@ -442,9 +451,9 @@ int main(int argc, char *argv[]) {
 	cvirt_oci_blob_destory(config_blob);
 	cvirt_oci_config_destroy(config);
 	cvirt_oci_layer_destroy(layer);
-	guestfs_umount_all(g);
-	guestfs_shutdown(g);
-	guestfs_close(g);
+	guestfs_umount_all(state.guestfs);
+	guestfs_shutdown(state.guestfs);
+	guestfs_close(state.guestfs);
 
 	return 0;
 }
