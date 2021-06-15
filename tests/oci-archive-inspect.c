8eb243cf8b35fda721674d51a99cb2b064553988
xdavidwu <xdavidwuph@gmail.com>
2021-06-15 22:50:29 +0800

    tests: add oci-archive-inspect

---
+++ tests/oci-archive-inspect.c
@@ -0,0 +1,39 @@
+#include <archive.h>
+#include <archive_entry.h>
+#include <convirter/oci-r/index.h>
+#include <convirter/oci-r/layer.h>
+#include <convirter/oci-r/manifest.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	if (argc != 2) {
+		exit(EXIT_FAILURE);
+	}
+	struct cvirt_oci_r_index *index = cvirt_oci_r_index_from_archive(argv[1]);
+	const char *manifest_digest = cvirt_oci_r_index_get_native_manifest_digest(index);
+	puts("manifest:");
+	puts(manifest_digest);
+	struct cvirt_oci_r_manifest *manifest = cvirt_oci_r_manifest_from_archive_blob(argv[1], manifest_digest);
+	const char *config_digest = cvirt_oci_r_manifest_get_config_digest(manifest);
+	puts("config:");
+	puts(config_digest);
+	puts("layers:");
+	int len = cvirt_oci_r_manifest_get_layers_length(manifest);
+	for (int i = 0; i < len; i++) {
+		const char *layer_digest = cvirt_oci_r_manifest_get_layer_digest(manifest, i);
+		puts(layer_digest);
+		struct cvirt_oci_r_layer *layer =
+			cvirt_oci_r_layer_from_archive_blob(argv[1], layer_digest,
+			cvirt_oci_r_manifest_get_layer_compression(manifest, i));
+		struct archive *archive = cvirt_oci_r_layer_get_libarchive(layer);
+		struct archive_entry *entry;
+		while (archive_read_next_header(archive, &entry) == ARCHIVE_OK) {
+			puts(archive_entry_pathname(entry));
+		}
+		cvirt_oci_r_layer_destroy(layer);
+	}
+	cvirt_oci_r_manifest_destroy(manifest);
+	cvirt_oci_r_index_destroy(index);
+	return 0;
+}
